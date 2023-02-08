ebe58f3f26507c4bbf5e397a2410308c78a05591
parkgyumin <wzwz123z@naver.com>
2023-02-08 22:22:45 +0900

    공부 커밋1

---
+++ src/main/java/org/example/P11720.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class P11720 {
+    public static void main(String[] args) {
+        //숫자의 합 구하기
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String sNum = sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum = 0;
+
+        for(int i=0; i < cNum.length; i++) {
+            // 문자열을 숫자형으로 바꿔줄때는 문자열 0을 마이너스로 해주면 되거나, -48을 해주면 됨 (아스키코드)
+            sum += cNum[i] - '0';
+        }
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/org/example/P1546.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class P1546 {
+}
