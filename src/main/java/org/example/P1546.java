2a341af68e623fab9f0cd1c8ec7b0a23c84c69c8
parkgyumin <wzwz123z@naver.com>
2023-02-12 22:16:29 +0900

    공부 커밋4

--- src/main/java/org/example/P1546.java
+++ src/main/java/org/example/P1546.java
@@ -12,6 +12,10 @@ public class P1546 {
         }
         long sum = 0;
         long max = 0;
-
+        for(int i = 0; i < N; i++) {
+            if(A[i] > max) max = A[i];
+            sum = sum + A[i];
+        }
+        System.out.println(sum * 100.0 / max / N);
     }
 }
