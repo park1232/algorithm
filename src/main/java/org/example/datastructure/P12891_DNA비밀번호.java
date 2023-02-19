28155fbd58d1ffea910017171f65d5f4f5311c85
parkgyumin <wzwz123z@naver.com>
2023-02-19 17:42:05 +0900

    P12891_DNA비밀번호2

--- src/main/java/org/example/datastructure/P12891_DNA비밀번호.java
+++ src/main/java/org/example/datastructure/P12891_DNA비밀번호.java
@@ -1,9 +1,113 @@
 package org.example.datastructure;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.rmi.Remote;
+import java.util.StringTokenizer;
 
 public class P12891_DNA비밀번호 {
+    static int myArr[];
+    static int checkArr[];
+    static int checkSecret;
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int S = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+        int result = 0;
+        checkArr = new int[4];
+        myArr = new int[4];
+        char A[] = new char[S];
+        checkSecret = 0;
+
+        A = br.readLine().toCharArray();
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < 4; i++) {
+            checkArr[i] = Integer.parseInt(st.nextToken());
+            if(checkArr[i] == 0) {
+                checkSecret++;
+            }
+        }
+       for(int i = 0; i < P; i++) { // 부분문자열 처음 받을때 세팅
+           Add(A[i]);
+       }
+
+       if(checkSecret == 4) { // 4자릿수와 관련된 크기가 모두 충족될때 유효한 비밀번호
+           result++;
+       }
+
+       // 슬라이딩 윈도우
+        for(int i=P; i < S; i++) {
+            int j = i-P;
+            Add(A[i]);
+            Remove(A[j]);
+
+            if(checkSecret == 4) {
+                result++;
+            }
+        }
+        System.out.println(result);
+        br.close();
+    }
+
+    private static void Remove(char c) { // 제거되는 문자를 처리하는 함수
+        switch (c) {
+            case 'A':
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret--;
+                }
+                myArr[0]--;
+                break;
+            case 'C':
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret--;
+                }
+                myArr[1]--;
+                break;
+            case 'G':
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret--;
+                }
+                myArr[2]--;
+                break;
+            case 'T':
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret--;
+                }
+                myArr[3]--;
+                break;
+        }
+    }
+
+    private static void Add(char c) { // 새로 들어온 문자를 처리하는 함수
+        switch (c) {
+            case 'A':
+                myArr[0]++;
+                if (myArr[0] == checkArr[0]) {
+                    checkSecret++;
+                }
+                break;
+            case 'C':
+                myArr[1]++;
+                if (myArr[1] == checkArr[1]) {
+                    checkSecret++;
+                }
+                break;
+            case 'G':
+                myArr[2]++;
+                if (myArr[2] == checkArr[2]) {
+                    checkSecret++;
+                }
+                break;
+            case 'T':
+                myArr[3]++;
+                if (myArr[3] == checkArr[3]) {
+                    checkSecret++;
+                }
+                break;
+
+        }
     }
 }
