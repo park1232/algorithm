e164a200cb63397759f932180861ce949f8823f7
parkgyumin <wzwz123z@naver.com>
2023-02-26 22:20:49 +0900

    P10989_수정렬하기3

--- src/main/java/org/example/sort/P10989_수정렬하기3.java
+++ src/main/java/org/example/sort/P10989_수정렬하기3.java
@@ -1,7 +1,30 @@
 package org.example.sort;
 
-public class P10989_수정렬하기3 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class P10989_수정렬하기3 {
+    public static void main(String[] args) throws IOException {
+        // 계수정렬
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+
+        for(int i = 0; i < N; i++) {
+            int number = Integer.parseInt(br.readLine());
+            count[number]++;
+        }
+        br.close();
+
+        for(int i = 0; i < 10000; i++) {
+            if(count[i] != 0) {
+                for(int j = 0; j < count[i]; j++) {
+                    bw.write(i + "\n");
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
     }
 }
